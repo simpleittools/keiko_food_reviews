15012b3ab22ec53870539521ef875f8477908f97
Ryan <wm.ryan.mooney@gmail.com>
2025-04-01 16:33:28 -0800

    setup dev branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  runApp(myApp);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
