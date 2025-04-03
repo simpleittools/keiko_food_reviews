813302df8b84d63d0183bb5f2538062d34581798
Ryan <wm.ryan.mooney@gmail.com>
2025-04-02 16:46:00 -0800

    remove theme test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:keiko_food_reviews/helper/themes.dart';
 import 'package:keiko_food_reviews/main.dart';
 import 'package:keiko_food_reviews/pages/home.dart';
 
@@ -22,14 +21,6 @@ void main() {
 
     // verify the home widget is rendered
     expect(find.byType(Home), findsOneWidget);
-
-    // test the MaterialApp's Theme and darkTheme
-    final MaterialApp materialApp = tester.widget(materialAppFinder);
-    expect(materialApp.theme, Themes.lightTheme());
-    expect(materialApp.theme, Themes.darkTheme());
-
-    // verify theme is set correctly
-    expect(materialApp.themeMode, ThemeMode.system);
 
     // verify the placeholder is rendering on the home widget
     expect(find.byType(Placeholder), findsOneWidget);
