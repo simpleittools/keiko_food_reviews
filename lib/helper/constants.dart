917ce8d2046a8d45387e5f612c2061681ca09293
Ryan <wm.ryan.mooney@gmail.com>
2025-04-02 17:11:33 -0800

    create constants file

---
+++ lib/helper/constants.dart
@@ -0,0 +1,37 @@
+enum DatabaseCollections {
+  users,
+  usersData,
+  reviews,
+}
+
+enum MenuItems {
+  add,
+  signout,
+}
+
+enum MenuItemsPhoto {
+  camera,
+  gallery,
+}
+
+enum ReviewMode {
+  add,
+  edit,
+  readOnly,
+}
+
+enum Affordability {
+  $,
+  $$,
+  $$$,
+  $$$$,
+}
+
+enum ResponsiveSizes {
+  mobile(600.0),
+  weDesktopTablet(601.0);
+
+  const ResponsiveSizes(this.value);
+
+  final double value;
+}
